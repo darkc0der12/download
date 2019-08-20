76edb0061a75334c10d759e83a1b2c0f7b50ecea
darkc0der12 <blackknightstd@gmail.com>
2019-08-20 16:12:41 +0545

    Update ads.js

--- ads.js
+++ ads.js
@@ -2,7 +2,3 @@ let e=document.createElement('div');
 e.id='ZfuzveXJDmGo';
 e.style.display='none';
 document.body.appendChild(e);
-
-if (!document.getElementById('ZfuzveXJDmGo')) {
-    document.getElementById('adblock-blocker-overlay').style.display = 'block';
-}

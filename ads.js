b3d0f6bebc01bbd185e1a3c216c114508d0e140e
darkc0der12 <blackknightstd@gmail.com>
2019-08-21 08:26:05 +0545

    Update ads.js

--- ads.js
+++ ads.js
@@ -2,8 +2,3 @@ var e=document.createElement('div');
 e.id='ZfuzveXJDmGo';
 e.style.display='none';
 document.body.appendChild(e);
-
-var link = document.createElement('a');
-link.textContent = 'Powered by Sombex.com';
-link.href = 'http://sombex.com';
-document.getElementById('#adblock-blocker-overlay').appendChild(link);
